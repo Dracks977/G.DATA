6bb4119a8d9cc65074ac825a40c90bd28fabb467
Dracks <eclat.nathan@gmail.com>
2018-07-13 00:40:00 +0200

    begining of api call

--- src/character.js
+++ src/character.js
@@ -1,10 +1,29 @@
 module.exports = function(app, path, ejs, fs, esso, cypher, rp){
 	app.get('/character', function(req, res){
-		console.log(req.query.id)
+		var info = new Object();
 		rp('https://esi.evetech.net/latest/characters/'+req.query.id+'/?datasource=tranquility').then(function (htmlString) {
-        		console.log(htmlString)
-        	}).catch(function (err) {
-       		 // Crawling failed...
-       		});
-        });
+			info.basic = JSON.parse(htmlString)
+			rp('https://esi.evetech.net/latest/characters/'+req.query.id+'/corporationhistory/?datasource=tranquility').then(function (htmlString) {
+				info.corpHistory = JSON.parse(htmlString)
+				rp('https://esi.evetech.net/latest/characters/'+req.query.id+'/portrait/?datasource=tranquility').then(function (htmlString) {
+					info.img = JSON.parse(htmlString)
+					fs.readFile(path.resolve(__dirname + '/../public/views/character.html'), 'utf-8', function(err, content) {
+						if (err) {
+							res.end('error occurred' + err);
+							return;
+							//cree la node du perso alliance et corpo
+						} 
+   						let renderedHtml = ejs.render(content, {data: info});  //get redered HTML code
+   						res.end(renderedHtml);
+   					});
+				}).catch(function (err) {
+
+				});
+			}).catch(function (err) {
+
+			});
+		}).catch(function (err) {
+
+		});
+	});
 }
